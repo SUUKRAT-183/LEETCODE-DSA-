932add51d04eb5da0b8771d1747324324590e9a5
SUUKRAT-183 <sagrawal2_be24@thapar.edu>
2026-08-19 22:58:38 +0530

    Update two-sum.cpp

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,6 +4,7 @@ public:
         int i,j,n=nums.size();
         for(i=0;i<n-1;i++){
             for(j=i+1;j<n;j++){
+                
                 if(nums[i]+nums[j]==target){
                     return {i, j};
                 }
